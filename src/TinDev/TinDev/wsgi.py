d6da0f79b108267243fedc92410ad4fa52cbe957
begloff <begloff@nd.edu>
2022-12-04 23:51:21 -0500

    Change file structure

--- src/TinDev/TinDev/wsgi.py
+++ src/TinDev/TinDev/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TinDev.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TinDev.settings")
 
 application = get_wsgi_application()
